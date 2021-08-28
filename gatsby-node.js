797e86909cd3e38ee32a198b105c85e795ff5221
peterbaile <peterchenwflms@gmail.com>
2021-08-28 17:36:48 -0400

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,50 +6,50 @@
 
 // You can delete this file if you're not using it
 
-const path = require('path')
+// const path = require('path')
 
-exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ actions, graphql }) => {
+//   const { createPage } = actions
 
-  let res = await graphql(`
-    query {
-      allMarkdownRemark(filter: {frontmatter: {path: {regex: "/work/"}}}) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
+//   let res = await graphql(`
+//     query {
+//       allMarkdownRemark(filter: {frontmatter: {path: {regex: "/work/"}}}) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: path.resolve('src/pages/templates/workPostTemp.js'),
-    })
-  })
+//   res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: path.resolve('src/pages/templates/workPostTemp.js'),
+//     })
+//   })
 
-  res = await graphql(`
-    query {
-      allMarkdownRemark(filter: {frontmatter: {path: {regex: "/blogs/"}}}) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `)
+//   res = await graphql(`
+//     query {
+//       allMarkdownRemark(filter: {frontmatter: {path: {regex: "/blogs/"}}}) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 
-  res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: path.resolve('src/pages/templates/blogPostTemp.js'),
-    })
-  })
-}
+//   res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//     createPage({
+//       path: node.frontmatter.path,
+//       component: path.resolve('src/pages/templates/blogPostTemp.js'),
+//     })
+//   })
+// }
