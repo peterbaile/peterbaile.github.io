541d7f90d079f6d72b28ab17e511e90771034a0b
Peter Chen <peterbailec@gmail.com>
2024-06-12 23:38:49 -0400

    update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,18 @@ const AboutPage = () => (
           </div>
           <div>
             <p style={{ marginBottom: '2em' }}>
+              Hi! I am Peter (陳百樂), a second-year PhD student at <a href="https://www.csail.mit.edu/">MIT CSAIL</a>, co-advised by <a href="https://www.csail.mit.edu/person/michael-cafarella">Mike Cafarella</a> and <a href="https://www.csail.mit.edu/person/michael-stonebraker">Mike Stonebraker</a>. I graduated from
+              the <a href="https://www.upenn.edu/">University of Pennsylvania</a> with a BSE degree. I am supported by <a href="https://scholars.croucher.org.hk/scholars/baile-chen">the Croucher scholarship</a>.
+              <br />
+              <br />
+              I am interested in improving the performance of LLMs in the context of information retrieval and complex reasoning. I also work on Machine Learning systems and data management.
+            </p>
+
+            <h4>News</h4>
+            <ul>
+              <li>(May 2024) Our paper on <a href="https://arxiv.org/abs/2404.09889">join-aware multi-table retrieval</a> was accepted to ACL 2024.</li>
+            </ul>
+            {/* <p style={{ marginBottom: '2em' }}>
               Hi! I am Peter, a second-year PhD student at <a href="https://www.csail.mit.edu/">MIT CSAIL</a>, co-advised by <a href="https://www.csail.mit.edu/person/michael-cafarella">Mike Cafarella</a> and <a href="https://www.csail.mit.edu/person/michael-stonebraker">Mike Stonebraker</a>. I graduated Summa Cum Laude from&nbsp;
               the <a href="https://www.upenn.edu/">University of Pennsylvania</a> with a BSE degree in&nbsp;
               <a href="https://www.nets.upenn.edu/">Networked &#38; Social Systems Engineering (NETS)</a>.
@@ -53,15 +65,15 @@ const AboutPage = () => (
               <a href="https://www.cis.upenn.edu/~zives/">Professor Zack Ives</a>,
               building ML systems.
               Prior to this, I worked with <a href="https://www.cis.upenn.edu/~sokolsky/">Professor Oleg Sokolsky</a> from the <a href="https://precise.seas.upenn.edu/">PRECISE Center</a> on software analysis tools. I was also a member of the <a href="https://dsl.cis.upenn.edu/">Penn Distributed Systems Lab</a>, working with <a href="https://www.cis.upenn.edu/~sga001/">Professor Sebastian Angel</a> and <a href="https://vincen.tl/">Professor Vincent Liu</a>.
-              {/* Outside of research, I am the student instructor of&nbsp;
+              Outside of research, I am the student instructor of&nbsp;
               <a href="https://www.seas.upenn.edu/~cis197/">CIS 197</a>&nbsp;and
               a teaching assistant for&nbsp;
-              <a href="https://www.cis.upenn.edu/~cis455/">CIS 455/555</a>. */}
+              <a href="https://www.cis.upenn.edu/~cis455/">CIS 455/555</a>. 
               <br />
               <br />
               Things I do when I am free &#61;
               🏃‍♂️&nbsp;&nbsp;&#43;&nbsp;&nbsp;🏊&nbsp;&nbsp;&#43;&nbsp;&nbsp;🚴‍♂️
-            </p>
+            </p> */}
             {/* <a
                 className="btn btn-outline-info"
                 href="/files/resume.pdf"
