9372d71627ee6fd8591353750e0c29f6d0e464e9
Peter Chen <peterbailec@gmail.com>
2025-04-11 10:51:10 -0400

    analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,26 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtag-script"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-HJMLC60W00"
+    />,
+    <script
+      key="gtag-init"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HJMLC60W00');
+        `,
+      }}
+    />,
+  ])
+}
